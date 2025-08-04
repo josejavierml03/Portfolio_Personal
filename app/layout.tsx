8d4ec256db5c320b06f5e8c6ca6bb07422985225
José Javier Méndez <josejavierml03@gmail.com>
2025-08-04 04:47:47 +0200

    2.2

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import { GeistSans } from 'geist/font/sans'
 import { GeistMono } from 'geist/font/mono'
+import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import './globals.css'
 
 export const metadata: Metadata = {
@@ -24,7 +26,10 @@ html {
 }
         `}</style>
       </head>
-      <body>{children}</body>
+      <body>{children}
+        <Analytics />
+        <SpeedInsights />
+      </body>
     </html>
   )
 }
